c5ed31a483105e196b96a92a79aeed24c993cd08
Mickael <mickael233@gmail.com>
2023-11-23 19:18:57 +0200

    feat: add toastify

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,18 @@
 import { Button } from '@mui/material';
+import { toast } from 'react-toastify';
+import { useMount } from 'react-use';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
+  useMount(() => {
+    toast.success('Hello World!');
+  });
+
   return (
     <>
       <Button>Good</Button>
+      <ToastContainer />
     </>
   );
 };
